dc08a09991704d1971c2c47b25bc46285bb57484
seong0210 <dlgustjd9566@gmail.com>
2022-09-26 10:45:31 +0900

    전략 패턴 기본

---
+++ src/strategy/App.java
@@ -0,0 +1,14 @@
+package strategy;
+
+
+
+public class App {
+	public static void main(String[] args) {
+		CoffeeMachine m = new CoffeeMachine();
+		m.brew(new Americano());
+		m.brew(new CafeLatte());
+		m.brew(new Espresso());// OCP
+
+	}
+
+}
---
+++ src/strategy/CoffeeMachine.java
@@ -0,0 +1,7 @@
+package strategy;
+
+public class CoffeeMachine {
+	public void brew(Coffee coffee) {
+		System.out.println(coffee.make());
+	}
+}
